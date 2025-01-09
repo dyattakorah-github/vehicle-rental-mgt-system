b777f0fe5982a210d4243dd6de5bf6159f31bfeb
dyattakorah-github <dyattakorah@st.ug.edu.gh>
2025-01-09 14:49:44 +0000

    Add EngineType enum to categorize motorcycle engines

--- src/main/java/vrs/models/enums/EngineType.java
+++ src/main/java/vrs/models/enums/EngineType.java
@@ -1,4 +1,11 @@
 package vrs.models.enums;
 
 public enum EngineType {
+    SINGLE_CYLINDER,
+    TWIN_CYLINDER,
+    TRIPLE_CYLINDER,
+    FOUR_CYLINDER,
+    SIX_CYLINDER,
+    ELECTRIC,
+    ROTARY
 }
